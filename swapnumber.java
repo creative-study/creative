ef6b05fa765aac0386585fa4b4e0df5125f215d3
creative-study <58321498+creative-study@users.noreply.github.com>
2019-12-27 12:49:37 +0530

    Program to swap two numbers

---
+++ swapnumber.java
@@ -0,0 +1,19 @@
+class swap
+{
+  public static void main(String args[])
+  {
+    int a, b;
+    a=10;
+    b=20;
+    System.out.println("Before Swapping : ");
+    System.out.println("A : "+a);
+    System.out.println("B : "+b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    System.out.println("After Swapping : ");
+    System.out.println("A : "+a);
+    System.out.println("B : "+b);
+    
+  }
+}
